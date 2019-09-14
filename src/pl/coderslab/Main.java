3200a760a448798d54db30c00bbbb415922f58d4
JakubFlorczak <florczakjakub@wp.pl>
2019-09-14 14:16:16 +0200

    stworzona gra

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,8 +1,36 @@
 package pl.coderslab;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randonNumber = random.nextInt(10);
+        System.out.println("Zgadnij liczbę:");
+        while (!scanner.hasNextInt()){
+            scanner.next();
+            System.out.println("Nie podałeś liczby. Spróbuj jeszcze raz:");
+        }
+        int userNumber = scanner.nextInt();
+        boolean result = false;
+        while (result == false) {
+            if (randonNumber == userNumber){
+                result = true;
+                System.out.println("Zgadłeś!");
+                }else if (randonNumber > userNumber) {
+                    result = false;
+                    System.out.println("Za mało! Zgaduj dalej:");
+                    userNumber = scanner.nextInt();
+                } else if (randonNumber < userNumber) {
+                     result = false;
+                    System.out.println("Za dużo! Zgaduj dalej:");
+                    userNumber = scanner.nextInt();
+                }
+        }
     }
 }
